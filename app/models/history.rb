e6d1e4ae8a250147d06f4cba58fd62be202b6349
shiraishono <157781350+shiraishono@users.noreply.github.com>
2024-04-22 21:02:21 +0900

    商品一覧表示機能ver2

--- app/models/history.rb
+++ app/models/history.rb
@@ -1,7 +1,7 @@
 class History < ApplicationRecord
 
-  belongs_to :user
-  belongs_to :item
+  #belongs_to :user
+  #belongs_to :item
 
   #has_one    :purchase
 
--- app/models/item.rb
+++ app/models/item.rb
@@ -9,7 +9,7 @@ class Item < ApplicationRecord
 
   belongs_to :user
   has_one_attached :image
-  has_one :history
+  #has_one :history
 
   # バリデーション
   validates :product_category_id, presence: true, numericality: { other_than: 1, message: "can't be blank" }
