45339489d55431667da85f007128d7a9098a14a8
shiraishono <157781350+shiraishono@users.noreply.github.com>
2024-04-22 20:17:13 +0900

    商品一覧表示機能ver1

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,2 +1,69 @@
 class ItemsController < ApplicationController
+
+  #before_action :authenticate_user!, only: [:new, :edit ,:destroy]
+  #before_action :move_to_index, only: [:edit]
+  #before_action :move_to_index_sold_out, only: [:edit]
+  before_action :set_item, only: [:edit, :show, :update, :destroy]
+  before_action :authenticate_user!, only: [:new]
+
+  def index
+    @items = Item.all.order("created_at DESC")
+  end
+
+  def new
+    @item = Item.new
+  end
+
+  def create
+    @item = Item.new(item_params)
+    if @item.save
+      redirect_to root_path, notice: '商品が正常に登録されました。'
+    else
+      # ステータスコード422 (Unprocessable Entity) を付与してnewテンプレートを再表示
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  #def show
+  #end
+
+  #def edit
+  #end
+
+  #def update
+    #if @item.update(item_params)
+      #redirect_to item_path(@item.id)
+    #else
+      #render :edit
+    #end
+  #end
+
+  #def destroy
+    #@item.destroy
+    #redirect_to root_path
+  #end
+
+  private
+
+  def item_params
+    params.require(:item).permit(:product_name,:product_description,:product_category_id,:product_condition_id,:shipping_charge_id,:region_of_origin_id,:shipping_day_id,:selling_price,:image).merge(user_id: current_user.id)
+  end
+
+  #def move_to_index
+    #@item = Item.find(params[:id])
+    #unless user_signed_in? && current_user == @item.user
+      #redirect_to action: :index
+    #end
+  #end
+
+  #def move_to_index_sold_out
+    #@item = Item.find(params[:id])
+    #if @item.buy.present?
+      #redirect_to action: :index
+    #end
+  #end
+
+  #def set_item
+    #@item = Item.find(params[:id])
+  #end
 end
---
+++ app/models/item.rb
@@ -0,0 +1,30 @@
+class Item < ApplicationRecord
+  # アソシエーション
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to_active_hash :product_category
+  belongs_to_active_hash :product_condition
+  belongs_to_active_hash :shipping_charge
+  belongs_to_active_hash :region_of_origin
+  belongs_to_active_hash :shipping_day
+
+  belongs_to :user
+  has_one_attached :image
+  has_one :history
+
+  # バリデーション
+  validates :product_category_id, presence: true, numericality: { other_than: 1, message: "can't be blank" }
+  validates :product_condition_id, presence: true, numericality: { other_than: 1, message: "can't be blank" }
+  validates :shipping_charge_id, presence: true, numericality: { other_than: 1, message: "can't be blank" }
+  validates :region_of_origin_id, presence: true, numericality: { other_than: 1, message: "can't be blank" }
+  validates :shipping_day_id, presence: true, numericality: { other_than: 1, message: "can't be blank" }
+
+  validates :product_name, presence: true
+  validates :product_description, presence: true
+  validates :selling_price, presence: { message: "can't be blank" },
+                          numericality: { only_integer: true,
+                                          greater_than_or_equal_to: 300,
+                                          less_than_or_equal_to: 9999999,
+                                          message: "is not a valid price" }
+
+  validates :image, presence: true
+end
